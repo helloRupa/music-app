ce9f6548e5e3b15a370e13eb8462dc4caaaea822
Rupa Dhillon <rupa211984@gmail.com>
2019-05-08 17:20:45 -0700

    Add album routes, model and assocs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
+
   resource :session, only: [:new, :create, :destroy]
+
   resources :bands
+
+  resources :bands, only: [:show] do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:create, :edit, :update, :show, :destroy]
 
   root 'sessions#new'
 end
