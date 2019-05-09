a5e45ca92ffb51244bc2741674a56c92b88400cb
Rupa Dhillon <rupa211984@gmail.com>
2019-05-08 22:56:25 -0700

    Destroy dependent data

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,7 +1,8 @@
 class Band < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many :albums
+  has_many :albums,
+    dependent: :destroy
 
   has_many :tracks,
     through: :albums,
